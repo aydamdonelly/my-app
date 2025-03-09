2eb38b927d60317124b2a555c9022c1a86aefb10
Adam <wi23315@lehre.dhbw-stuttgart.de>
2025-03-09 17:50:47 +0100

    Updates

--- src/setupTests.js
+++
@@ -1,3 +0,0 @@
-// jest-dom fügt benutzerdefinierte Jest-Matcher hinzu, mit denen du DOM-Knoten testen kannst.
-// Weitere Infos: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
